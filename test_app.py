04a9ff38f750582d6aa0e7277c12c9ac9299232c
Pranathi2307 <pranathi704@gmail.com>
2024-11-30 17:36:34 +0530

    unit test cases

--- test_app.py
+++
@@ -1,5 +0,0 @@
-from app import add
-def test_appp():
-    assert add(2,3)==5
-    assert add(1,7)==8
-    assert add(-1,1)==0
---
+++ tests/test_operations.py
@@ -0,0 +1,6 @@
+from source.math_add import add
+def test_add():
+    assert add(2,3)==5
+    assert add(1,-1)==0
+    assert add(9,0)==9
+
